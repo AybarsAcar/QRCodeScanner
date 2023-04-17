91f7271c7c4ef8ffe1934660d5903c99f6977bef
Aybars Acar <aybars.dev@gmail.com>
2023-04-17 19:24:05 +1000

    Initial Commit

---
+++ QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by Aybars Acar on 17/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
